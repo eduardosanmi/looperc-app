953aaeca94cd183ac0c8d3b5bb1c932b47b4b39c
eduardosanmi <eduardosanmi@users.noreply.github.com>
2025-03-02 14:09:22 +0000

    Genre slection changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,37 +2,43 @@ import streamlit as st
 import numpy as np
 from storedb import query_by_payload
 from sampler import gen_audio, genres
-import time
-
-avail_albums = query_by_payload(genre = genres, limit = 1000)
+import time, random
 
 st.title("l:blue[OO]perc")
 st.subheader(":grey[Jam :blue[fresh] new tracks...]", divider="gray")
 
-sel_selected_album = st.selectbox("selection",
-                                  list(avail_albums[0].album.unique()),
+sel_selected_genre = st.selectbox("selection",
+                                  genres,
                                   index=None,
                                   label_visibility = 'collapsed')
-if sel_selected_album is None:
+if sel_selected_genre is None:
     st.warning("Please select an option to continue.")
     st.stop()
 
-if st.button("Jam!", type="primary"):
-    with st.spinner("Generating track, this can take a while... ...", show_time=True): 
-        data_wav, sections, durations = gen_audio(
-            album = sel_selected_album, n_steps = 6)
+avail_albums = query_by_payload(genre = [sel_selected_genre], limit = 1000)
+if avail_albums:
+    sel_selected_album = random.choice(list(avail_albums[0].album.unique()))
+    if sel_selected_album:
+        jam_button = st.button("Jam!", type="primary")
+        if jam_button:
+            with st.spinner("Generating track, this can take a while... ...", show_time=True): 
+                data_wav, sections, durations = gen_audio(
+                    album = sel_selected_album, n_steps = 6)
+            st.audio(np.array(data_wav).T, sample_rate=44100, loop = False, autoplay = True)
+            with st.empty(): 
+                time.sleep(.2)
+                for i in range(len(sections)):
+                    columns = st.columns((1, 1, 1)) 
+                    with columns[1]:
+                        st.subheader(f''':gray[Playing :green[{sections[i]}] for {round(durations[i],1)} secs ]''')
+                        if i < len(sections)-1:
+                            st.write(f''':gray[Next :green[{sections[i+1]}] for {round(durations[i+1],1)} secs ]''')
+                        time.sleep(durations[i]) 
+        sel_selected_album = None                   
+else:
+    jam_button = st.button("Jam!", type="secondary", disabled=True)
+    sel_selected_album = None
 
-    st.audio(np.array(data_wav).T, sample_rate=44100, loop = False, autoplay = True)
-    with st.empty(): 
-        time.sleep(.2)
-        for i in range(len(sections)):
-            columns = st.columns((1, 1, 1)) 
-            with columns[1]:
-                st.subheader(f''':gray[Playing :green[{sections[i]}] for {round(durations[i],1)} secs ]''')
-                if i < len(sections)-1:
-                    st.write(f''':gray[Next :green[{sections[i+1]}] for {round(durations[i+1],1)} secs ]''')
-                time.sleep(durations[i])    
-    
 
         
     
