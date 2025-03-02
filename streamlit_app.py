727de0d8890ce3d4ba0ab5619d64b90fb9713118
eduardosanmi <eduardosanmi@users.noreply.github.com>
2025-03-02 13:06:47 +0000

    added button

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,20 +17,21 @@ if sel_selected_album is None:
     st.warning("Please select an option to continue.")
     st.stop()
 
-with st.spinner("Generating track, this can take a while... ...", show_time=True): 
-    data_wav, sections, durations = gen_audio(
-        album = sel_selected_album, n_steps = 6)
+if st.button("Jam!", type="primary"):
+    with st.spinner("Generating track, this can take a while... ...", show_time=True): 
+        data_wav, sections, durations = gen_audio(
+            album = sel_selected_album, n_steps = 6)
 
-st.audio(np.array(data_wav).T, sample_rate=44100, loop = False, autoplay = True)
-with st.empty(): 
-    time.sleep(.2)
-    for i in range(len(sections)):
-        columns = st.columns((1, 1, 1)) 
-        with columns[1]:
-            st.subheader(f''':gray[Playing :green[{sections[i]}] for {round(durations[i],1)} secs ]''')
-            if i < len(sections)-1:
-                st.write(f''':gray[Next :green[{sections[i+1]}] for {round(durations[i+1],1)} secs ]''')
-            time.sleep(durations[i])    
+    st.audio(np.array(data_wav).T, sample_rate=44100, loop = False, autoplay = True)
+    with st.empty(): 
+        time.sleep(.2)
+        for i in range(len(sections)):
+            columns = st.columns((1, 1, 1)) 
+            with columns[1]:
+                st.subheader(f''':gray[Playing :green[{sections[i]}] for {round(durations[i],1)} secs ]''')
+                if i < len(sections)-1:
+                    st.write(f''':gray[Next :green[{sections[i+1]}] for {round(durations[i+1],1)} secs ]''')
+                time.sleep(durations[i])    
     
 
         
